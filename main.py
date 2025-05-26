de2fb677a1d711c0b73a01fa19132c4b01d1faaa
cesargodoy <cesar@03.cl>
2025-05-26 16:54:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def home():
 @app.route("/api/noticias", methods=["GET"])
 def noticias_json():
     fecha = datetime.now().strftime("%Y-%m-%d")
-    path = f"data/noticias_{fecha}.json"
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(BASE_DIR, f"data/noticias_{fecha}.json")
     if os.path.exists(path):
         with open(path, "r", encoding="utf-8") as f:
             data = json.load(f)
@@ -31,13 +32,15 @@ def procesar_y_guardar():
         n['resumen'] = resumen if resumen else "[Error al resumir]"
         noticias_con_resumen.append(n)
 
+    # Asegura que el directorio data/ exista
     os.makedirs("data", exist_ok=True)
+
     nombre_archivo = f"data/noticias_{datetime.now().strftime('%Y-%m-%d')}.json"
     with open(nombre_archivo, 'w', encoding='utf-8') as f:
         json.dump(noticias_con_resumen, f, ensure_ascii=False, indent=2)
 
-    print(f"Archivo guardado: {nombre_archivo}")
+    print(f"✅ Archivo guardado: {nombre_archivo}")
 
 if __name__ == "__main__":
     procesar_y_guardar()
-    app.run(host="0.0.0.0", port=10000)  # Render escucha por el puerto 10000
+    app.run(host="0.0.0.0", port=10000)
